a93bf1529d30edfd9780a97536cda02e59fe9e7f
GuilhermeVRF <guilhermevr.figueiredo@outlook.com>
2025-05-24 17:08:49 -0300

    feat: implementa serviço de usuário com CRUD completo

---
+++ internal/service/user_service.go
@@ -0,0 +1,45 @@
+package service
+
+import (
+    "user-service/internal/model"
+    "user-service/internal/repository"
+
+    "github.com/google/uuid"
+)
+
+type UserService interface {
+    Create(user *model.User) error
+    GetByID(id string) (*model.User, error)
+    GetAll() ([]model.User, error)
+    Update(user *model.User) error
+    Delete(id string) error
+}
+
+type userService struct {
+    userRepository repository.UserRepository
+}
+
+func NewUserService(userRepository repository.UserRepository) UserService {
+    return &userService{userRepository}
+}
+
+func (userService *userService) Create(user *model.User) error {
+    user.ID = uuid.New()
+    return userService.userRepository.Create(user)
+}
+
+func (userService *userService) GetByID(id string) (*model.User, error) {
+    return userService.userRepository.FindByID(id)
+}
+
+func (userService *userService) GetAll() ([]model.User, error) {
+    return userService.userRepository.FindAll()
+}
+
+func (userService *userService) Update(user *model.User) error {
+    return userService.userRepository.Update(user)
+}
+
+func (userService *userService) Delete(id string) error {
+    return userService.userRepository.Delete(id)
+}
