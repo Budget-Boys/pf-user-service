3fb0d402e4a9e370ac68e78b6146147afdd687fd
GuilhermeVRF <guilhermevr.figueiredo@outlook.com>
2025-05-29 22:13:59 -0300

    refactor: aplica got fmt em user_update_input.go

--- internal/dto/user_update_input.go
+++ internal/dto/user_update_input.go
@@ -1,9 +1,9 @@
 package dto
 
 type UserUpdateInput struct {
-	Name     string `json:"name" validate:"required,min=3"`
-	CPFCNPJ  string `json:"cpfcnpj" validate:"required,len=11|len=14"`
+	Name     string `json:"name" validate:"min=3"`
+	CPFCNPJ  string `json:"cpfcnpj" validate:"len=11|len=14"`
 	Phone    string `json:"phone"`
-	Email    string `json:"email" validate:"required,email"`
-	Password string `json:"password" validate:"required,min=6"`
+	Email    string `json:"email" validate:"email"`
+	Password string `json:"password" validate:"min=6"`
 }
