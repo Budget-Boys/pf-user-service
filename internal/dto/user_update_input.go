371a7b83c7320bfded6fd7b9ede462efd64fca68
GuilhermeVRF <guilhermevr.figueiredo@outlook.com>
2025-06-02 20:07:12 -0300

    refactor: atualizar UserUpdateInput fields para lidar quando o usuario não mandar email ou cpfcnpj na requisição

--- internal/dto/user_update_input.go
+++ internal/dto/user_update_input.go
@@ -2,8 +2,8 @@ package dto
 
 type UserUpdateInput struct {
 	Name     string `json:"name" validate:"min=3"`
-	CPFCNPJ  string `json:"cpfcnpj" validate:"len=11|len=14"`
+	CPFCNPJ  string `json:"cpfcnpj,omitempty" validate:"omitempty,len=11|len=14"`
 	Phone    string `json:"phone"`
-	Email    string `json:"email" validate:"email"`
+	Email    string `json:"email" validate:"omitempty,email"`
 	Password string `json:"password" validate:"min=6"`
 }
