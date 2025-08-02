a30f1bc7d77d338b2a7e0ac3fb35cab681f8a89a
GuilhermeVRF <guilhermevr.figueiredo@outlook.com>
2025-08-02 10:27:12 -0300

    refactor: update validation rules in UserUpdateInput struct to allow omitempty

--- internal/dto/user_update_input.go
+++ internal/dto/user_update_input.go
@@ -1,9 +1,10 @@
 package dto
 
 type UserUpdateInput struct {
-	Name     string `json:"name" validate:"min=3"`
-	CPFCNPJ  string `json:"cpfcnpj,omitempty" validate:"omitempty,len=11|len=14"`
+	Name     string `json:"name" validate:"omitempty,min=3"`
+	CPFCNPJ  string `json:"cpfcnpj" validate:"omitempty,len=11|len=14"`
 	Phone    string `json:"phone"`
 	Email    string `json:"email" validate:"omitempty,email"`
-	Password string `json:"password" validate:"min=6"`
+	Password string `json:"password" validate:"omitempty,min=6"`
 }
+
