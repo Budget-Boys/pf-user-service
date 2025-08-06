b04ecaf6cbaff7dcfaaa693af59f9735b31e8e88
GuilhermeVRF <guilhermevr.figueiredo@outlook.com>
2025-08-06 20:42:08 -0300

    feat: add CORS middleware to allow frontend access and update server port to 8080

--- cmd/main.go
+++ cmd/main.go
@@ -10,7 +10,7 @@ import (
 	"user-service/internal/service"
 
 	"os"
-
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
 )
@@ -43,6 +43,12 @@ func main() {
 
 	app := fiber.New()
 
+	 app.Use(cors.New(cors.Config{
+        AllowOrigins:     "http://localhost:5173", // ou o domínio do seu frontend
+        AllowCredentials: true,
+        AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
+        AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS",
+    }))
 	app.Post("/users", h.Create)
 	app.Get("/users", h.GetAll)
 	app.Get("/users/:id", h.GetByID)
@@ -53,6 +59,6 @@ func main() {
 
 	app.Get("/ping-redis", handler.PingRedis)
 
-	log.Fatal(app.Listen(":9000"))
+	log.Fatal(app.Listen(":8080"))
 
 }
--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.23.4
 require (
 	github.com/ArthurHlt/go-eureka-client v1.1.0
 	github.com/go-playground/validator/v10 v10.11.0
-	github.com/gofiber/fiber/v2 v2.52.8
+	github.com/gofiber/fiber/v2 v2.52.9
 	github.com/golang-jwt/jwt/v4 v4.5.2
 	github.com/google/uuid v1.6.0
 	github.com/joho/godotenv v1.5.1
